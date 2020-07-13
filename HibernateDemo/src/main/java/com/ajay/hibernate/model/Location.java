a04c8519eb41be61dc5767ab5c4d20b5e2cf549a
Ajay Narayan Shinde <63417102+ShindeAjay238@users.noreply.github.com>
2020-07-13 22:40:13 +0530

    hibernate Demo useing h2 Database

---
+++ HibernateDemo/src/main/java/com/ajay/hibernate/model/Location.java
@@ -0,0 +1,41 @@
+package com.ajay.hibernate.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Location
+{
+	@Id
+	private Integer id;
+	private String name;
+	public Location()
+	{
+
+	}
+
+	public Location(Integer id, String name) 
+	{
+	
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
